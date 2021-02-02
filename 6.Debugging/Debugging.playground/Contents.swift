5b359e7eb745ae9e0bb5607cf03d8c4139d1b8e8
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-02 18:07:42 -0300

    add handle events function to debugging operators

--- 6.Debugging/Debugging.playground/Contents.swift
+++ 6.Debugging/Debugging.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Combine
 
+var cancellable: AnyCancellable?
+
 func applyPrint() {
     let publisher = (0...13).publisher
     publisher
@@ -8,4 +10,24 @@ func applyPrint() {
         .sink { let _ = $0 }
 }
 
-applyPrint()
+func applyHandleEvents() {
+    guard let url = URL(string: "https://my-json-server.typicode.com/typicode/demo/posts") else {
+        fatalError("Invalid URL")
+    }
+    
+    let request = URLSession.shared.dataTaskPublisher(for: url)
+    
+    cancellable = //Comment this line if you want to see the request being canceled, and how is it printed.
+        request.handleEvents(receiveSubscription: { _ in print("received subscription") },
+                                            receiveOutput: { _, _ in print("received output") },
+                                            receiveCompletion: { _ in print("received completion") },
+                                            receiveCancel: { print("received cancel") },
+                                            receiveRequest: { _ in print("received request") })
+        .sink(receiveCompletion: { print($0) }) { data, response in
+            print(data)
+        }
+    
+}
+
+//applyPrint()
+applyHandleEvents()
