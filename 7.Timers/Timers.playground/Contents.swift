6e935eb9ce9440c5009bde7473c342b2ff1eb54d
lapedro.veloso <pedrofveloso@gmail.com>
2021-02-04 10:20:44 -0300

    add timer class function to timer opeators; add some comments to runLoop function

--- 7.Timers/Timers.playground/Contents.swift
+++ 7.Timers/Timers.playground/Contents.swift
@@ -12,10 +12,23 @@ func useRunLoops() {
         print("Event fired.")
     } //remember that when the function you're calling returns a cancellable object, you must hold it in some variable or constant, in order to receive the events.
     
-    runLoop.schedule(after: runLoop.now.advanced(by: .seconds(5))) {
+    runLoop.schedule(after: runLoop.now.advanced(by: .seconds(5))) { //you also can use '.init(Date(timeIntervalSinceNow: 5)' as a value for the after: parameter.
         cancellable?.cancel()
         print("Event canceled.")
     }
 }
 
-useRunLoops()
+func useTimers() {
+    let unit = TimeInterval(2)
+    let subscription = Timer.publish(every: unit, on: .main, in: .common).autoconnect()
+    
+    cancellable = subscription
+        .scan(0) { current, _ in
+                current + unit}
+        .sink {
+        print($0)
+    }
+}
+
+//useRunLoops()
+useTimers()
